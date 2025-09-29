349096882d218f33c049efc579b294f4cdbe3e27
lyrehcAu <aurl.cheryl@gmail.com>
2025-09-29 23:24:23 +0700

    Contact

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ function createLeaf() {
 
 // Keep generating leaves
 setInterval(createLeaf, 500);
-
 const header = document.querySelector('header');
 window.addEventListener('scroll', function() {
     if (window.scrollY > 50) {
@@ -29,39 +28,6 @@ window.addEventListener('scroll', function() {
       } else {
         header.classList.remove('scrolled'); // hapus class scrolled kalo balik atas lagi
     }
-});
-
-const learnMoreButton = document.querySelector('.learn-more-button');
-let intervalId;
-
-// sparkle generator
-function createRandomSparkle() {
-  const sparkle = document.createElement('span');
-  sparkle.classList.add('sparkle');
-  sparkle.innerHTML = '✨'; // sparklenya emot
-
-  // spawn sparkle random
-  const x = Math.random() * learnMoreButton.offsetWidth;
-  const y = Math.random() * learnMoreButton.offsetHeight;
-  sparkle.style.left = x + 'px';
-  sparkle.style.top = y + 'px';
-
-  learnMoreButton.appendChild(sparkle);
-
-  // sparkle dihapus setelah animasi done -> 2 detik
-  setTimeout(() => {
-    sparkle.remove();
-  }, 2000);
-}
-
-// kalo button dihover -> spawn sparkle tiap 300ms
-learnMoreButton.addEventListener('mouseenter', () => {
-  intervalId = setInterval(createRandomSparkle, 300);
-});
-
-// kalau not hover lagi -> stop spawn
-learnMoreButton.addEventListener('mouseleave', () => {
-  clearInterval(intervalId);
 });
 
 document.addEventListener("DOMContentLoaded", () => {
